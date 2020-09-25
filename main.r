ad5b18e074ee69ed8439c92d807787333eafa3e1
luca-terrazzan <luca.terraz@gmail.com>
2020-09-25 18:57:50 +0200

    other linear stuff

--- main.r
+++ main.r
@@ -14,5 +14,21 @@ linearRegression <- lm(people)
 print(linearRegression)
 
 # Scatterplot
-plot(people)
-abline(linearRegression)
+# plot(people, abline(linearRegression))
+
+plot(people, main = "People data",
+     pch = 19, frame = TRUE)
+abline(lm(y ~ x), col = "blue")
+
+predictedWeight <- predict(linearRegression, 170)
+
+
+# Create the predictor and response variable.
+x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+relation <- lm(y~x)
+
+# Plot the chart.
+plot(y,x,col = "blue",main = "Height & Weight Regression",
+abline(lm(x~y)),cex = 1.3,pch = 16,xlab = "Weight in Kg",ylab = "Height in cm")
+
